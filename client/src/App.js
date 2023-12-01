5591c2ff70c9a592412a1a9a2bfbd4d747af644e
Landon Heaney <landonheaney@gmail.com>
2023-11-30 21:26:44 -0600

    Created basic front-end for task management app

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState({ name: '', description: '', status: '' });
+
+  const addTask = () => {
+    if (newTask.name && newTask.description) {
+      setTasks([...tasks, { ...newTask, status: 'Pending' }]);
+      setNewTask({ name: '', description: '', status: '' });
+    }
+  };
+
+  const updateTaskStatus = (index, newStatus) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, status: newStatus } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Task Manager</h1>
+        <div>
+          <input
+            type="text"
+            value={newTask.name}
+            onChange={(e) => setNewTask({ ...newTask, name: e.target.value })}
+            placeholder="Task Name"
+          />
+          <input
+            type="text"
+            value={newTask.description}
+            onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
+            placeholder="Task Description"
+          />
+          <button onClick={addTask}>Add Task</button>
+        </div>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <div className="task-detail">
+                <strong>Task Name:</strong> {task.name}<br/>
+                <strong>Task Description:</strong> {task.description}<br/>
+                <strong>Status:</strong> {task.status}
+              </div>
+              <div className="buttons">
+                <button onClick={() => deleteTask(index)}>Delete</button>
+                {task.status !== 'Completed' && (
+                  <button onClick={() => updateTaskStatus(index, 'Completed')}>
+                    Mark as Completed
+                  </button>
+                )}
+                {task.status !== 'Pending' && (
+                  <button onClick={() => updateTaskStatus(index, 'Pending')}>
+                    Mark as Pending
+                  </button>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
